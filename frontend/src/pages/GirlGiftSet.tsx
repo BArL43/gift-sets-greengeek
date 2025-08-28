b73a32befff0533d47cf6f1a3d26f2e05a959c93
BArL43 <144413501+BArL43@users.noreply.github.com>
2025-08-28 13:06:31 +0300

    Add files via upload

--- frontend/src/pages/GirlGiftSet.tsx
+++ frontend/src/pages/GirlGiftSet.tsx
@@ -28,7 +28,6 @@ const girlGiftSet = {
   price: 1190,
   images: [
     '/photo_2025-07-25_13-55-20.jpg',
-    '/photo_2025-07-25_14-22-23.jpg',
     '/косметичка.jpg',
     '/скраб_белый.jpg',
     '/маски.jpg',
